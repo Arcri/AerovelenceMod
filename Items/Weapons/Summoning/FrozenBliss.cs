4067a5b4d3901cb0bde1cf85c647d253f5da734c
Arcri <47931960+Arcri@users.noreply.github.com>
2020-05-30 19:35:39 -0600

    Lots of new stuff
    wee

---
+++ Items/Weapons/Summoning/FrozenBliss.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.GameContent;
+using Terraria.IO;
+using Terraria.ObjectData;
+using Terraria.Utilities;
+using Terraria.ModLoader;
+
+namespace AerovelenceMod.Items.Weapons.Summoning
+{
+    public class FrozenBliss : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Frozen Bliss");
+            Tooltip.SetDefault("It's just a bunch of spirits. Nothing too much to worry about");
+        }
+
+        public override void SetDefaults()
+        {
+            item.mana = 8;
+            item.damage = 26;
+            item.useStyle = 1;
+            item.width = 32;
+            item.height = 32;
+            item.useTime = 16;
+            item.useAnimation = 16;
+            item.noMelee = true;
+            item.knockBack = 1f;
+            item.rare = ItemRarityID.Pink;
+            item.value = Item.sellPrice(0, 2, 0, 0);
+            item.UseSound = SoundID.Item8;
+            item.autoReuse = false;
+            item.shoot = mod.ProjectileType("IcyElementalist");
+            item.shootSpeed = 0f;
+            item.summon = true;
+		}
+    }
+}
---
+++ NPCs/Cave/CaveSnail.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AerovelenceMod.NPCs.Cave
+{
+	public class CaveSnail : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+	        DisplayName.SetDefault("Cave Snail");
+            Main.npcFrameCount[npc.type] = 5;
+		}
+        public override void SetDefaults()
+        {
+            npc.aiStyle = 67;
+            npc.lifeMax = 4;
+            npc.defense = 24;
+            npc.knockBackResist = 0f;
+            npc.width = 38;
+            npc.height = 18;
+            npc.value = Item.buyPrice(0, 0, 4, 50);
+            npc.lavaImmune = true;
+            npc.noGravity = false;
+            npc.noTileCollide = false;
+            npc.HitSound = SoundID.NPCHit2;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.buffImmune[24] = true;
+		}
+	}
+}
