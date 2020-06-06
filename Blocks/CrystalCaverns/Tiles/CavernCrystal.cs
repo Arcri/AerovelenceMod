2ef0ecea0da5ba18835856efd8201c8fcc5398ea
Arcri <47931960+Arcri@users.noreply.github.com>
2020-06-05 21:40:34 -0600

    minor edits
    haha laugh

--- Blocks/CrystalCaverns/Tiles/CavernCrystal.cs
+++ Blocks/CrystalCaverns/Tiles/CavernCrystal.cs
@@ -1,6 +1,8 @@
 using Terraria;
 using Terraria.ModLoader;
 using AerovelenceMod.Items.Placeble.CrystalCaverns;
+using System;
+using Terraria.ID;
 
 namespace AerovelenceMod.Blocks.CrystalCaverns.Tiles
 {
@@ -11,13 +13,18 @@ namespace AerovelenceMod.Blocks.CrystalCaverns.Tiles
 			mineResist = 2.5f;
 			minPick = 59;
             Main.tileSolid[Type] = true;
-            Main.tileMergeDirt[Type] = true;
-            Main.tileBlockLight[Type] = false;
-            Main.tileLighted[Type] = false;
+            Main.tileMerge[Type][ModContent.TileType<CavernStone>()] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = true;
 			dustType = 59;
 			soundType = 21;
 			drop = ModContent.ItemType<CavernCrystalItem>();
-
+		}
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)   //light colors
+        {
+            r = 0.0f;
+            g = 0.5f;
+            b = 0.9f;
         }
     }
 }
