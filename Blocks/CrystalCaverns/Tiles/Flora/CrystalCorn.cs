51f19e6a48128fb95a1d6d1e2013b11385a8b888
Arcri <gdarcri@gmail.com>
2021-02-10 01:22:25 -0700

    A Magical Place

---
+++ Blocks/CrystalCaverns/Tiles/Flora/CrystalCorn.cs
@@ -0,0 +1,38 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Terraria.Enums;
+
+namespace AerovelenceMod.Blocks.CrystalCaverns.Tiles.Flora
+{
+    public class CrystalCorn : ModTile
+	{
+		public override void SetDefaults()
+		{
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Crystal Corn");
+			Main.tileFrameImportant[Type] = true;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style2xX);
+			TileObjectData.newTile.Height = 4;
+			TileObjectData.newTile.Width = 1;
+			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 };
+			TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
+			TileObjectData.newTile.StyleWrapLimit = 2;
+			TileObjectData.newTile.StyleMultiplier = 2;
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
+			TileObjectData.addAlternate(1);
+			TileObjectData.addTile(Type);
+			dustType = -1;
+			AddMapEntry(new Color(209, 82, 203), name);
+		}
+		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height)
+		{
+			offsetY = 2;
+		}
+	}
+}
