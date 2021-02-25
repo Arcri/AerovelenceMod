3d7dae6df408dec957e4489390c2c6ac3e9f759c
Arcri <gdarcri@gmail.com>
2021-02-25 04:22:30 -0700

    s

--- NPCs/Bosses/CrystalTumbler/CrystalTumbler.cs
+++ NPCs/Bosses/CrystalTumbler/CrystalTumbler.cs
@@ -20,9 +20,17 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 		{
 			IdleRoll = 0,
 			ProjectileSpawn = 1,
-			SuperDash = 2
+			SuperDash = 2,
+			Electricity = 3,
+			RockRain = 4,
+			Jump = 5
 		}
 
+        public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
+        {
+            npc.lifeMax = 3200;  //boss life scale in expertmode
+            npc.damage = 20;  //boss damage increase in expermode
+        }
 		/// <summary>
 		/// Manages the current AI state of the Crystal Tumbler.
 		/// Gets and sets npc.ai[0] as tracker.
@@ -61,6 +69,7 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 		public bool P;
 		public int spinTimer;
 		public bool Phase2;
+		int i;
 		public int counter = 0;
 
 		public override void SetDefaults()
@@ -85,13 +94,6 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 			npc.DeathSound = SoundID.NPCDeath44;
 			music = mod.GetSoundSlot(SoundType.Music, "Sounds/Music/CrystalTumbler");
 		}
-
-		public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
-		{
-			npc.lifeMax = 3200;  //boss life scale in expertmode
-			npc.damage = 20;  //boss damage increase in expermode
-		}
-
 		public override bool PreAI()
 		{
 			npc.TargetClosest(true);
@@ -110,6 +112,7 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 				}
 			}
 
+
 			npc.noTileCollide = npc.noGravity = false;
 
 			// No second phase implemented yet.
@@ -124,13 +127,13 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 			{
 				RollingMove(target);
 
-				if (++AttackTimer >= 250)
+				if (++AttackTimer >= 200)
 				{
 					AttackTimer = 0;
 
 					if (Main.netMode != NetmodeID.MultiplayerClient)
 					{
-						State = (CrystalTumblerState)Main.rand.Next(1, 3);
+						State = (CrystalTumblerState)Main.rand.Next(1, 6);
 					}
 
 					npc.netUpdate = true;
@@ -143,25 +146,37 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 				// TODO: Eldrazi - Multiplayer support?
 				if (AttackTimer++ == 0)
 				{
+					Main.PlaySound(SoundID.Item, (int)npc.Center.X, (int)npc.Center.Y, 69, 0.75f);
 					Projectile.NewProjectile(npc.Center, default, ModContent.ProjectileType<TumblerBoulder1>(), 12, 1f, Main.myPlayer, 0);
 				}
 				else if (AttackTimer == 10)
 				{
+					Main.PlaySound(SoundID.Item, (int)npc.Center.X, (int)npc.Center.Y, 69, 0.75f);
 					Projectile.NewProjectile(npc.Center, default, ModContent.ProjectileType<TumblerBoulder1>(), 12, 1f, Main.myPlayer, 1);
 				}
 				else if (AttackTimer == 20)
 				{
+					Main.PlaySound(SoundID.Item, (int)npc.Center.X, (int)npc.Center.Y, 69, 0.75f);
 					Projectile.NewProjectile(npc.Center, default, ModContent.ProjectileType<TumblerBoulder1>(), 12, 1f, Main.myPlayer, 2);
 
-					AttackTimer = 0;
-					State = CrystalTumblerState.IdleRoll;
-				}
 
+				}
 				RollingMove(target);
 			}
 			// Attack state. Starts speedy roll in place. After a set amount of ticks, releases/dashes with a high velocity and frequent jumps/bounces.
+			else if (State == CrystalTumblerState.Jump)
+			{
+
+				npc.velocity.Y -= Main.rand.Next(12) + 7;
+				for (int num325 = 0; num325 < 20; num325++)
+				{
+					Dust.NewDust(npc.position, npc.width, npc.height, DustID.Electric, npc.velocity.X, npc.velocity.Y, 0, default, 1);
+				}
+				State = CrystalTumblerState.IdleRoll;
+			}
 			else if (State == CrystalTumblerState.SuperDash)
 			{
+				CheckPlatform(target);
 				// Rotating in place state.
 				if (++AttackTimer <= 180)
 				{
@@ -178,31 +193,64 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 				// Fast horizontal movement and frequent jumps.
 				else
 				{
+					Main.PlaySound(SoundID.Item, (int)npc.Center.X, (int)npc.Center.Y, 76, 0.75f);
 					TryJump(Main.rand.Next(4, 7), 60);
 					JumpTimer--;
-
-					if (npc.velocity.X <= 20 * npc.direction)
+					var player = Main.player[npc.target];
+					if (player.Center.X > npc.Center.X)
 					{
-						npc.velocity.X += 0.3f * npc.direction;
-
-						if (Math.Sign(npc.velocity.X) != npc.direction)
-						{
-							npc.velocity.X += 0.2f * npc.direction;
-						}
+						npc.velocity.X += 0.3f;
 					}
-
-					// After 480 ticks (300 ticks or 5 seconds after the 'Rotating in place' state), go back to idle rolling.
-					if (AttackTimer >= 480)
+					else if (player.Center.X < npc.Center.X)
 					{
-						npc.netUpdate = true;
-
-						AttackTimer = 0;
-						State = CrystalTumblerState.IdleRoll;
+						npc.velocity.X -= 0.3f;
 					}
-
-					npc.rotation += npc.velocity.X * 0.025f;
 				}
+
+
+				// After 480 ticks (300 ticks or 5 seconds after the 'Rotating in place' state), go back to idle rolling.
+				if (AttackTimer >= 480)
+				{
+					npc.netUpdate = true;
+
+					AttackTimer = 0;
+					State = CrystalTumblerState.IdleRoll;
+				}
+
+				npc.rotation += npc.velocity.X * 0.025f;
 			}
+
+			else if (State == CrystalTumblerState.Electricity)
+			{
+				var player = Main.player[npc.target];
+				Vector2 vector8 = new Vector2(npc.position.X + (npc.width * 0.5f), npc.position.Y + (npc.height * 0.5f));
+				if (AttackTimer++ == 0)
+				{
+					Main.PlaySound(SoundID.Item, (int)npc.Center.X, (int)npc.Center.Y, 93, 0.75f);
+					float Speed = 1 * 0.99f;
+					int damage = Main.expertMode ? 50 : 40;// if u want to change this, 15 is for expert mode, 10 is for normal mod
+					int type = mod.ProjectileType("TumblerOrb");
+					float rotation = (float)Math.Atan2(vector8.Y - (player.position.Y + (player.height * 0.5f)), vector8.X - (player.position.X + (player.width * 0.5f)));
+					Projectile.NewProjectile(vector8.X, vector8.Y, (float)(Math.Cos(rotation) * Speed * -1), (float)(Math.Sin(rotation) * Speed * -1), type, damage, 0f, 0);
+				}
+				AttackTimer = 0;
+				State = CrystalTumblerState.IdleRoll;
+				RollingMove(target);
+			}
+			else if (State == CrystalTumblerState.RockRain)
+			{
+				Main.PlaySound(SoundID.Item, (int)npc.Center.X, (int)npc.Center.Y, 74, 0.75f);
+				var player = Main.player[npc.target];
+				for (int i = -3; i <= 3; i++)
+				{
+					Projectile.NewProjectile(player.Center.X + i * 20, player.Center.Y - 380, i * 2, -2, ModContent.ProjectileType<TumblerSpike1>(), 30, 0f, Main.myPlayer, 0f, 0f);
+					Projectile.NewProjectile(player.Center.X + i * 20, player.Center.Y - 380, i * 2, -2, ModContent.ProjectileType<TumblerSpike2>(), 30, 0f, Main.myPlayer, 0f, 0f);
+				}
+				AttackTimer = 0;
+				State = CrystalTumblerState.Jump;
+				RollingMove(target);
+			}
+
 
 			/*if ((npc.Center.Y - player.Center.Y) < -150)
 			{
@@ -212,32 +260,18 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 			{
 				i = 0;
 			}
+>>>>>>> Stashed changes
 
-            if (i % 100 == 0)
-            {
-                SuperDash = true;
-            }
-
-            if (SuperDash == true)
-            {
-                counter++;
-                if (counter >= 60)
+                if (SuperDash == true)
                 {
-                    if (player.Center.X > npc.Center.X)
-                    {
-                        npc.velocity.X += -1.1f;
-                    }
-                    else if (player.Center.X < npc.Center.X)
-                    { 
-                        npc.velocity.X += 1.1f;
-                    }
-                    counter = 0;
-                }
-                else
-                {
-                    SuperDash = false;
+@ -336,6 +585,211 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
+                    npc.timeLeft = 10;
                 }
             }
+<<<<<<< Updated upstream
+        }
+    }
+=======
 
 			if (State == CrystalTumblerState.Phase2)
 			{
@@ -279,7 +313,31 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 
 			return (false);
 		}
+		private void CheckPlatform(Player player)
+		{
+			i++;
+			bool onplatform = true;
 
+			for (int i = (int)npc.position.X; i < npc.position.X + npc.width; i += npc.width / 4)
+			{
+				Tile tile = Framing.GetTileSafely(new Point((int)npc.position.X / 16, (int)(npc.position.Y + npc.height + 8) / 16));
+				if (!TileID.Sets.Platforms[tile.type])
+				{
+					onplatform = false;
+				}
+			}
+			if (onplatform && (npc.position.Y + npc.height + 20 < player.Center.Y))
+			{
+				if (i % 70 == 0)
+				{
+					npc.noTileCollide = true;
+				}
+				else
+				{
+					npc.noTileCollide = false;
+				}
+			}
+		}
 		public override void NPCLoot()
 		{
 			if (Main.expertMode)
@@ -360,6 +418,7 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 		{
 			// Movement.
 			float desiredSpeed = 2 + 12 * (1 - LifePercentLeft);
+			CheckPlatform(player);
 			if (player.Center.X > npc.Center.X)
 			{
 				if (npc.velocity.X < desiredSpeed)
--- NPCs/Bosses/CrystalTumbler/TumberBoulders.cs
+++ NPCs/Bosses/CrystalTumbler/TumberBoulders.cs
@@ -3,26 +3,15 @@ using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-<<<<<<< Updated upstream
 using static Terraria.ModLoader.ModContent;
-=======
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
->>>>>>> Stashed changes
 
 namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 {
 	public class TumblerBoulder1 : ModProjectile
 	{
-<<<<<<< Updated upstream
-		int t;
-		public override void SetDefaults()
-		{
-			projectile.aiStyle = 1;
-			projectile.width = 44;
-			projectile.height = 44;
-=======
 		private NPC owner => Main.npc[NPC.FindFirstNPC(ModContent.NPCType<CrystalTumbler>())];
         public override void SetStaticDefaults()
         {
@@ -33,8 +22,6 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 		{
 			projectile.width = 60;
 			projectile.height = 46;
-
->>>>>>> Stashed changes
 			projectile.alpha =  255;
 			projectile.damage = 12;
 			projectile.friendly = false;
@@ -42,19 +29,6 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 			projectile.tileCollide = false;
 			projectile.ignoreWater = true;
 		}
-<<<<<<< Updated upstream
-		public override void AI()
-		{
-			projectile.alpha -= 10;
-			projectile.velocity.Y *= 99.8f;
-			t++;
-			projectile.velocity *= 1.01f;
-			int dust1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.Black, 1);
-			Main.dust[dust1].velocity /= 2f;
-			if (t > 25)
-			{
-				projectile.tileCollide = true;
-=======
 		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
 			if (isntThrown == true)
@@ -97,77 +71,8 @@ namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
 
 					projectile.netUpdate = true;
 				}
->>>>>>> Stashed changes
 			}
+			return true;
 		}
 	}
-}
-
-namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
-{
-	public class TumblerBoulder2 : ModProjectile
-	{
-		int t;
-		public override void SetDefaults()
-		{
-			projectile.aiStyle = 1;
-			projectile.width = 44;
-			projectile.height = 44;
-			projectile.alpha = 255;
-			projectile.damage = 12;
-			projectile.friendly = false;
-			projectile.hostile = true;
-			projectile.tileCollide = false;
-			projectile.ignoreWater = true;
-		}
-		public override void AI()
-		{
-			projectile.alpha -= 10;
-			projectile.velocity.Y *= 99.8f;
-			t++;
-			projectile.velocity *= 1.01f;
-			int dust1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.Black, 1);
-			Main.dust[dust1].velocity /= 2f;
-			if (t > 25)
-			{
-				projectile.tileCollide = true;
-			}
-		}
-	}
-<<<<<<< Updated upstream
-}
-
-namespace AerovelenceMod.NPCs.Bosses.CrystalTumbler
-{
-	public class TumblerBoulder3 : ModProjectile
-	{
-		int t;
-		public override void SetDefaults()
-		{
-			projectile.aiStyle = 1;
-			projectile.width = 44;
-			projectile.height = 44;
-			projectile.damage = 12;
-			projectile.alpha = 255;
-			projectile.friendly = false;
-			projectile.hostile = true;
-			projectile.tileCollide = false;
-			projectile.ignoreWater = true;
-		}
-		public override void AI()
-		{
-			projectile.alpha -= 10;
-			projectile.velocity.Y *= 99.8f;
-			t++;
-			projectile.velocity *= 1.01f;
-			int dust1 = Dust.NewDust(projectile.position, projectile.width, projectile.height, DustID.Blood, projectile.velocity.X, projectile.velocity.Y, 0, Color.Black, 1);
-			Main.dust[dust1].velocity /= 2f;
-			if (t > 25)
-			{
-				projectile.tileCollide = true;
-			}
-		}
-	}
-=======
->>>>>>> Stashed changes
 }
