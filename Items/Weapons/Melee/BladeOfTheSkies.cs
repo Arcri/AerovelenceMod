ec4b2a0f3df8200db8c8632005076ae21dd6b2f9
Arcri <gdarcri@gmail.com>
2021-03-15 04:35:30 -0600

    Lightning Moth Drops

---
+++ Items/Weapons/Melee/BladeOfTheSkies.cs
@@ -0,0 +1,32 @@
+using AerovelenceMod.Items.Others.Crafting;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AerovelenceMod.Items.Weapons.Melee
+{
+    public class BladeOfTheSkies : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blade of the Skies");
+            Tooltip.SetDefault("Hitting enemies releases shards of crystal around the enemy that will implode");
+        }
+        public override void SetDefaults()
+        {
+            item.crit = 4;
+            item.damage = 24;
+            item.melee = true;
+            item.width = 46;
+            item.height = 46;
+            item.useTime = 25;
+            item.useAnimation = 25;
+            item.UseSound = SoundID.Item1;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.knockBack = 5;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.rare = ItemRarityID.Green;
+            item.autoReuse = false;
+        }
+    }
+}
---
+++ Items/Weapons/Summoning/SongOfTheStorm.cs
@@ -0,0 +1,45 @@
+using AerovelenceMod.Buffs;
+using AerovelenceMod.Projectiles.Minions;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AerovelenceMod.Items.Weapons.Summoning
+{
+    public class SongOfTheStorm : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Song of the Storm");
+            Tooltip.SetDefault("Summons a baby lightning moth that will charge at enemies and blast enemies with electricity");
+        }
+
+        public override void SetDefaults()
+        {
+            item.mana = 8;
+            item.damage = 26;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.width = 64;
+            item.height = 64;
+            item.useTime = 16;
+            item.useAnimation = 16;
+            item.noMelee = true;
+            item.knockBack = 1f;
+            item.rare = ItemRarityID.Pink;
+            item.value = Item.sellPrice(0, 2, 0, 0);
+            item.UseSound = SoundID.Item8;
+            item.autoReuse = false;
+            item.shoot = ModContent.ProjectileType<BurningNeutronStar>();
+            item.shootSpeed = 0f;
+            item.summon = true;
+            item.buffType = ModContent.BuffType<BurnshineStaffBuff>();
+		}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            player.AddBuff(item.buffType, 2);
+            position = Main.MouseWorld;
+            return true;
+        }
+    }
+}
