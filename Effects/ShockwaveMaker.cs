770e54c61b8bd5f529786b90f3e45f31f525a091
KingYellowFace <64530236+KingYellowFace@users.noreply.github.com>
2021-03-20 23:16:20 +0800

    some updates to shockwave maker

--- Effects/ShockwaveMaker.cs
+++ Effects/ShockwaveMaker.cs
@@ -1,12 +1,7 @@
-using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Terraria;
 using Terraria.Graphics.Effects;
-using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace Aerovelence.Effects
+namespace AerovelenceMod.Effects
 {
     public class ShockwaveMaker : ModProjectile
     {
@@ -15,26 +10,24 @@ namespace Aerovelence.Effects
         private int rippleSize = 15;
         private int rippleSpeed = 15;
         private float distortStrength = 300f;
-
         public override string Texture => "AerovelenceMod/Blank";
 
         public override void SetDefaults()
         {
             projectile.width = 4;
             projectile.height = 4;
+
             projectile.timeLeft = 400;
             projectile.alpha = 255;
+        }
 
-        }
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("trol");
-        }
         public override void AI()
         {
-            if (projectile.ai[0] == 0)
+            projectile.ai[0] += 8;
+
+            if (projectile.ai[1] == 0)
             {
-                projectile.ai[0] = 1;
+                projectile.ai[1] = 1;
                 if (!Filters.Scene["Shockwave"].IsActive())
                 {
                     Filters.Scene.Activate("Shockwave", projectile.Center).GetShader().UseColor(rippleCount, rippleSize, rippleSpeed).UseTargetPosition(projectile.Center);
@@ -42,9 +35,9 @@ namespace Aerovelence.Effects
             }
             else
             {
-                projectile.ai[0]++;
+                projectile.ai[1]++;
                 float progress = projectile.ai[1] / 60f;
-                float distortStrength = 300;
+                float distortStrength = 200;
                 Filters.Scene["Shockwave"].GetShader().UseProgress(progress).UseOpacity(distortStrength * (1 - progress / 3f));
             }
         }
