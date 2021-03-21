f9c4087a9bf2f3c8f6c6a4274b07ebe29d23fb9a
Arcri <gdarcri@gmail.com>
2021-03-21 01:21:13 -0600

    Fixed rock collector sprite

--- Content/NPCs/CrystalCaverns/LuminousDefender.cs
+++ Content/NPCs/CrystalCaverns/LuminousDefender.cs
@@ -38,7 +38,7 @@ namespace AerovelenceMod.Content.NPCs.CrystalCaverns
 		public override void PostDraw(SpriteBatch spriteBatch, Color drawColor)
 		{
 			Texture2D texture = mod.GetTexture("Content/NPCs/CrystalCaverns/LuminousDefender_Glow");
-			spriteBatch.Draw(texture, npc.Center - Main.screenPosition, npc.frame, Color.White, npc.rotation, npc.frame.Size() / 2f, npc.scale, SpriteEffects.None, 0);
+			spriteBatch.Draw(texture, npc.Center - Main.screenPosition, npc.frame, npc.GetAlpha(Color.White), npc.rotation, npc.frame.Size() / 2f, npc.scale, SpriteEffects.None, 0);
 		}
 
 		public override void HitEffect(int hitDirection, double damage)
