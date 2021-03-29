157dacae357718e255bd9d93cbfcfd5f1d737c52
Arcri <gdarcri@gmail.com>
2021-03-28 21:38:19 -0600

    Update Spitfire.cs

--- Content/Items/Weapons/Ranged/Spitfire.cs
+++ Content/Items/Weapons/Ranged/Spitfire.cs
@@ -12,23 +12,23 @@ namespace AerovelenceMod.Content.Items.Weapons.Ranged
 		}
         public override void SetDefaults()
         {
-			item.UseSound = SoundID.Item5;
-			item.crit = 9;
-            item.damage = 34;
-            item.magic = true;
-            item.mana = 5;
+			item.UseSound = SoundID.Item12;
+			item.crit = 8;
+            item.damage = 50;
+            item.reuseDelay = 10;
+            item.ranged = true;
             item.width = 30;
             item.height = 54;
-            item.useTime = 22;
-			item.useAnimation = 22;
+            item.useTime = 5;
+			item.useAnimation = 10;
             item.useStyle = ItemUseStyleID.HoldingOut;
             item.noMelee = true;
             item.knockBack = 4;
-            item.value = Item.sellPrice(0, 2, 50, 0);
+            item.value = Item.sellPrice(0, 5, 0, 0);
             item.rare = ItemRarityID.Pink;
             item.autoReuse = true;
             item.shoot = ProjectileID.ScutlixLaserFriendly;
-            item.shootSpeed = 7f;
+            item.shootSpeed = 16f;
         }
     }
 }
