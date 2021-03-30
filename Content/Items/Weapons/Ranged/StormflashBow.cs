cc1e2979fde1e5e60136384d0bba7c5252193eb2
Exitium <luaresgames@gmail.com>
2021-03-30 15:30:05 -0300

    Stormflash Bow

--- Content/Items/Weapons/Ranged/StormflashBow.cs
+++ Content/Items/Weapons/Ranged/StormflashBow.cs
@@ -1,4 +1,5 @@
 using AerovelenceMod.Content.Items.Others.Crafting;
+using AerovelenceMod.Content.Projectiles.Weapons.Ranged;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
@@ -11,57 +12,34 @@ namespace AerovelenceMod.Content.Items.Weapons.Ranged
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Stormflash Bow");
+            Tooltip.SetDefault("Converts Wooden Arrows into Shocking ones");
         }
         public override void SetDefaults()
         {
-            item.UseSound = SoundID.Item5;
-            item.crit = 20;
-            item.damage = 42;
-            item.ranged = true;
             item.width = 32;
             item.height = 56;
+            item.damage = 42;
+            item.knockBack = 2;
+            item.crit = 20;
             item.useTime = 18;
             item.useAnimation = 18;
-            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.shootSpeed = 18f;
+            item.ranged = true;
             item.noMelee = true;
-            item.knockBack = 2;
-            item.value = 10000;
-            item.rare = ItemRarityID.Yellow;
             item.autoReuse = true;
+            item.UseSound = SoundID.Item5;
             item.shoot = AmmoID.Arrow;
             item.useAmmo = AmmoID.Arrow;
-            item.shootSpeed = 21f;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.rare = ItemRarityID.LightRed;
         }
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            float numberProjectiles = 2 + Main.rand.Next(1);
-            float rotation = MathHelper.ToRadians(20);
-            position += Vector2.Normalize(new Vector2(speedX, speedY)) * 45f;
-            for (int i = 0; i < numberProjectiles; i++)
+            if (type == ProjectileID.WoodenArrowFriendly)
             {
-                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(15));
-                float scale = 1f - (Main.rand.NextFloat() * .3f);
-                if (i == 1)
-                {
-                    Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X * 2, perturbedSpeed.Y * 2, type, damage, knockBack, player.whoAmI);
-                    Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ProjectileID.FrostburnArrow, damage, knockBack, player.whoAmI);
-                }
-                else
-                {
-                    Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X * 2, perturbedSpeed.Y * 2, type, damage, knockBack, player.whoAmI);
-                }
+                type = ModContent.ProjectileType<ShockingArrow>();
             }
-            return false;
-        }
-        public override void AddRecipes()
-        {
-            ModRecipe modRecipe = new ModRecipe(mod);
-            modRecipe.AddIngredient(ModContent.ItemType<FrostShard>(), 8);
-            modRecipe.AddIngredient(ItemID.IceBlock, 35);
-            modRecipe.AddIngredient(ItemID.HellstoneBar, 10);
-            modRecipe.AddTile(TileID.Anvils);
-            modRecipe.SetResult(this, 1);
-            modRecipe.AddRecipe();
+            return true;
         }
     }
 }
