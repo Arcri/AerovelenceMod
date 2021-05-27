13b1e672dcc7cd609d97c3ddbf1969a1c58cffac
Lemmy-Koopa <kennithvolcy@gmail.com>
2021-05-26 21:44:33 -0400

    not finished, broken

--- Content/Items/Weapons/Melee/StarglassDagger.cs
+++ Content/Items/Weapons/Melee/StarglassDagger.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -25,6 +27,16 @@ namespace AerovelenceMod.Content.Items.Weapons.Melee
             item.value = Item.sellPrice(0, 3, 50, 0);
             item.rare = ItemRarityID.Green;
             item.autoReuse = true;
+        }
+
+        public override void UseStyle(Player player)
+        {
+            float cosRot = (float)Math.Cos(player.itemRotation - 0.78f * player.direction * player.gravDir);
+            float sinRot = (float)Math.Sin(player.itemRotation - 0.78f * player.direction * player.gravDir);
+            for (int i = 0; i < 1; i++)
+            {
+                float length = (item.width * 1.2f - i * item.width / 9) * item.scale - 4; //length to base + arm displacemen
+            }
         }
     }
 }
