b224c5fdd783ba807eb2efa1ae445e4e372e1224
Arcri <gdarcri@gmail.com>
2021-09-17 14:28:23 -0600

    gay sword

--- Content/Items/Weapons/Melee/ElementalShift.cs
+++ Content/Items/Weapons/Melee/ElementalShift.cs
@@ -30,8 +30,8 @@ namespace AerovelenceMod.Content.Items.Weapons.Melee
             item.value = 10000;
             item.autoReuse = true;
             item.rare = ItemRarityID.Orange;
-            item.shoot = item.shoot = mod.ProjectileType("ElementScythe");
-            item.shootSpeed = 5f;
+            item.shoot = item.shoot = ModContent.ProjectileType<ElementScythe>();
+            item.shootSpeed = 3;
             item.autoReuse = false;
         }
         public override void AddRecipes()
@@ -49,28 +49,6 @@ namespace AerovelenceMod.Content.Items.Weapons.Melee
             recipe.AddTile(TileID.Anvils);
             recipe.SetResult(this);
             recipe.AddRecipe();
-        }
-
-        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-        {
-            float numberProjectiles = 2 + Main.rand.Next(1);
-            float rotation = MathHelper.ToRadians(20);
-            position += Vector2.Normalize(new Vector2(speedX, speedY)) * 45f;
-            for (int i = 0; i < numberProjectiles; i++)
-            {
-                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(15));
-                float scale = 1f - (Main.rand.NextFloat() * .3f);
-                if (i == 1)
-                {
-                    Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ModContent.ProjectileType<ElementScythe>(), damage, knockBack, player.whoAmI);
-                    Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X * 1, perturbedSpeed.Y * 1, type, damage, knockBack, player.whoAmI);
-                }
-                else
-                {
-                    Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X * 1, perturbedSpeed.Y * 1, type, damage, knockBack, player.whoAmI);
-                }
-            }
-            return false;
         }
     }
 }
