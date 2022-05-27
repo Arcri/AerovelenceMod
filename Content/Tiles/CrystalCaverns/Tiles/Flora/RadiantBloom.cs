118ef648fe5b1c80e3e2c814dfcb9a844a49fde7
CraftyDWB <60546679+CraftyDWB@users.noreply.github.com>
2022-05-27 16:31:03 -0400

    Updated for TMod SoundType reimplementation

--- Content/Tiles/CrystalCaverns/Tiles/Flora/RadiantBloom.cs
+++ Content/Tiles/CrystalCaverns/Tiles/Flora/RadiantBloom.cs
@@ -17,7 +17,7 @@ namespace AerovelenceMod.Content.Tiles.CrystalCaverns.Tiles.Flora
 			Main.tileNoFail[Type] = true;
 			Main.tileMergeDirt[Type] = true;
 			DustType = 116;
-			SoundType = SoundID.Grass;
+			HitSound = SoundID.Grass;
 			AddMapEntry(new Color(100, 125, 255));
 			TileObjectData.addTile(Type);
 		}
