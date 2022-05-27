6334f97cd1c0d1f855e963a7b1d3148dbe4be01f
CraftyDWB <60546679+CraftyDWB@users.noreply.github.com>
2022-05-27 01:02:05 -0400

    Removed extra arg(s) causing error in PlaySound

--- Content/NPCs/Bosses/Decurion/DecurionBodyRocket1.cs
+++ Content/NPCs/Bosses/Decurion/DecurionBodyRocket1.cs
@@ -43,7 +43,7 @@ namespace AerovelenceMod.Content.NPCs.Bosses.Decurion
             distanceNorm.Normalize();
             if (i % Main.rand.Next(1000, 2000) == 0)
             {
-                SoundEngine.PlaySound(SoundID.Item, (int)NPC.Center.X, (int)NPC.Center.Y, 94, 0.75f);
+                SoundEngine.PlaySound(SoundID.Item, NPC.Center);
                 Projectile.NewProjectile(NPC.GetSource_FromAI(), NPC.Center.X, NPC.Center.Y, distanceNorm.X * 10, distanceNorm.Y * 10, ModContent.ProjectileType<DecurionRocket>(), 30, 0f, Main.myPlayer, 0f, 0f);
             }
 
