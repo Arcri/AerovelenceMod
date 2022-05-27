d1e9f0c28f8d130aef95522fb0df00ae931ef457
CraftyDWB <60546679+CraftyDWB@users.noreply.github.com>
2022-05-27 00:14:49 -0400

    Updated to include new 1.4 tree functions

--- Content/Tiles/CrystalCaverns/Tiles/CrystalTree.cs
+++ Content/Tiles/CrystalCaverns/Tiles/CrystalTree.cs
@@ -1,6 +1,9 @@
 using AerovelenceMod.Content.Dusts;
 using AerovelenceMod.Content.Items.Placeables.Furniture.Glimmering;
 using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.GameContent;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
@@ -8,7 +11,34 @@ namespace AerovelenceMod.Content.Tiles.CrystalCaverns.Tiles
 {
     public class CrystalTree : ModTree
 	{
-		private Mod mod => ModLoader.GetMod("AerovelenceMod");
+
+		// This is a blind copy-paste from Vanilla's PurityPalmTree settings.
+		public override TreePaintingSettings TreeShaderSettings => new TreePaintingSettings
+		{
+			UseSpecialGroups = true,
+			SpecialGroupMinimalHueValue = 11f / 72f,
+			SpecialGroupMaximumHueValue = 0.25f,
+			SpecialGroupMinimumSaturationValue = 0.88f,
+			SpecialGroupMaximumSaturationValue = 1f
+		};
+
+		public override void SetStaticDefaults()
+		{
+			// Makes Example Tree grow on ExampleBlock
+			GrowsOnTileId = new int[2] { ModContent.TileType<CrystalGrass>(), ModContent.TileType<LushGrowth>() };
+		}
+
+		public override int SaplingGrowthType(ref int style)
+		{
+			style = 0;
+			return ModContent.TileType<CrystalSapling>();
+		}
+
+		public override void SetTreeFoliageSettings(Tile tile, int xoffset, ref int treeFrame, ref int floorY, ref int topTextureFrameWidth, ref int topTextureFrameHeight)
+		{
+			// Copied from ExampleMod on 5/26/2022 at 11:36PM EST
+			// This function is for advanced stuff
+		}
 
 		public override int CreateDust()
 		{
@@ -17,7 +47,7 @@ namespace AerovelenceMod.Content.Tiles.CrystalCaverns.Tiles
 
 		public override int GrowthFXGore()
 		{
-			return mod.Find<ModGore>("Gores/ExampleTreeFX").Type;
+			return ModContent.Find<ModGore>("Gores/ExampleTreeFX").Type;
 		}
 
 		public override int DropWood()
@@ -25,19 +55,19 @@ namespace AerovelenceMod.Content.Tiles.CrystalCaverns.Tiles
 			return ItemType <Glimmerwood>();
 		}
 
-		public override Texture2D GetTexture()
+		public override Asset<Texture2D> GetTexture()
 		{
-			return mod.Assets.Request<Texture2D>("Content/Tiles/CrystalCaverns/Tiles/CrystalTree").Value;
+			return ModContent.Request<Texture2D>("Content/Tiles/CrystalCaverns/Tiles/CrystalTree");
 		}
 
-		public override Texture2D GetTopTextures(int i, int j, ref int frame, ref int frameWidth, ref int frameHeight, ref int xOffsetLeft, ref int yOffset)
+		public override Asset<Texture2D> GetTopTextures()
 		{
-			return mod.Assets.Request<Texture2D>("Content/Tiles/CrystalCaverns/Tiles/CrystalTree_Tops").Value;
+			return ModContent.Request<Texture2D>("Content/Tiles/CrystalCaverns/Tiles/CrystalTree_Tops");
 		}
 
-		public override Texture2D GetBranchTextures(int i, int j, int trunkOffset, ref int frame)
+		public override Asset<Texture2D> GetBranchTextures()
 		{
-			return mod.Assets.Request<Texture2D>("Content/Tiles/CrystalCaverns/Tiles/CrystalTree_Branches").Value;
+			return ModContent.Request<Texture2D>("Content/Tiles/CrystalCaverns/Tiles/CrystalTree_Branches");
 		}
 	}
 }
