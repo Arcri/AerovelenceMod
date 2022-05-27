33ece580f48bf2d31aeaf53651df7fbb1cfd9137
CraftyDWB <60546679+CraftyDWB@users.noreply.github.com>
2022-05-27 00:49:22 -0400

    Removed extra arg(s) causing error in PlaySound

--- Content/Items/Weapons/Melee/EquinoxProjectile.cs
+++ Content/Items/Weapons/Melee/EquinoxProjectile.cs
@@ -143,7 +143,7 @@ namespace AerovelenceMod.Content.Items.Weapons.Melee
 			{
 				Projectile.netUpdate = true;
 				Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
-				SoundEngine.PlaySound(SoundID.Dig, (int)Projectile.position.X, (int)Projectile.position.Y);
+				SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
 			}
 
 			return false;
