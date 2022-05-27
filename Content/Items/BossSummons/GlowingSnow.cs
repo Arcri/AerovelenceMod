5bc1af17df14dec3f2229f53690b8793cd402196
CraftyDWB <60546679+CraftyDWB@users.noreply.github.com>
2022-05-27 00:44:22 -0400

    Removed extra arg causing error in PlaySound

--- Content/Items/BossSummons/GlowingSnow.cs
+++ Content/Items/BossSummons/GlowingSnow.cs
@@ -34,7 +34,7 @@ namespace AerovelenceMod.Content.Items.BossSummons
             if (player.ZoneSnow)
                 NPC.SpawnOnPlayer(player.whoAmI, Mod.Find<ModNPC>("Rimegeist").Type);
             
-            SoundEngine.PlaySound(SoundID.Roar, (int)player.position.X, (int)player.position.Y, 0);
+            SoundEngine.PlaySound(SoundID.Roar, player.position);
 
             return true;
         }
