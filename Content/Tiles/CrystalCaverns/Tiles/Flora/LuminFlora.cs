568b8762a4b0ade32c3ca4eafb7032321abc32fe
CraftyDWB <60546679+CraftyDWB@users.noreply.github.com>
2022-05-27 16:15:37 -0400

    Updated for TMod SoundType reimplementation

--- Content/Tiles/CrystalCaverns/Tiles/Flora/LuminFlora.cs
+++ Content/Tiles/CrystalCaverns/Tiles/Flora/LuminFlora.cs
@@ -14,7 +14,7 @@ namespace AerovelenceMod.Content.Tiles.CrystalCaverns.Tiles.Flora
 			Main.tileNoFail[Type] = true;
 			Main.tileMergeDirt[Type] = true;
 			DustType = 116;
-			SoundType = SoundID.Grass;
+			HitSound = SoundID.Grass;
 			AddMapEntry(new Color(100, 125, 255));
 		}
 		public override void NumDust(int i, int j, bool fail, ref int num)
