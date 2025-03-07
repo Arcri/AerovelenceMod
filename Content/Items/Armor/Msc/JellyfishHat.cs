56a139b4da98894af68229e118c0a7e8e4514863
Locomotive2004 <jade.train04@gmail.com>
2025-03-07 02:19:10 -0600

    added defaults and recipe for jellyfish hat

---
+++ Content/Items/Armor/Msc/JellyfishHat.cs
@@ -0,0 +1,47 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace AerovelenceMod.Content.Items.Armor.Msc
+{
+    [AutoloadEquip(EquipType.Head)]
+    class JellyfishHat : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 18;
+            Item.height = 18;
+            Item.value = Item.sellPrice(gold: 1, silver: 10, copper: 5);
+            Item.rare = ItemRarityID.Blue;
+            Item.defense = 3;
+
+
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.BlueJellyfish)
+                .AddRecipeGroup(RecipeGroupID.IronBar, 4)
+                .AddTile(TileID.Anvils)
+                .Register();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
