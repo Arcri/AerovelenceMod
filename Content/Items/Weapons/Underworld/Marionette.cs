f72aa50d9d894ce3f38367bcf76c9ce383c216d0
Quarzz <130183472+Quarzz1@users.noreply.github.com>
2025-03-10 00:48:13 +0200

    verlettest removed, wip marionette

---
+++ Content/Items/Weapons/Underworld/Marionette.cs
@@ -0,0 +1,312 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AerovelenceMod.Content.Items.Weapons.Underworld
+{
+    public class Marionette : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = Item.height = 32;
+            Item.damage = 30;
+            Item.knockBack = 2f;
+            Item.DamageType = DamageClass.Summon;
+
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useTime = Item.useAnimation = 20;
+            Item.shoot = ModContent.ProjectileType<MarionetteProj>();
+
+            Item.rare = ItemRarityID.Orange;
+
+            Item.channel = true;
+            Item.noMelee = true;
+            Item.useTurn = true;
+            Item.noUseGraphic = true;
+        }
+    }
+
+    public class MarionetteProj : ModProjectile
+    {
+        public override string Texture => "Terraria/Images/Projectile_0";
+
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 32;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.DamageType = DamageClass.Summon;
+        }
+
+        #region swingPhysicVars
+        float barsRot;
+        Vector2 stillPos;
+        Vector2 barLoc = Vector2.Zero;
+        float barSpeed = 10f;
+        float acc = 0f;
+        float accBack = 0f;
+        float accActualBack = 0.002f;
+        bool rotStop = false;
+        int rememberDir;
+        #endregion
+
+        Vector2 verlet1EndPos = Vector2.Zero;
+        float verlet1Acc = 0f;
+
+        public override void AI()
+        {
+            Player p = Main.player[Projectile.owner];
+
+            if (barLoc == Vector2.Zero)
+            {
+                barLoc = Main.MouseWorld;
+            }
+
+            if (p.channel)
+            {
+                Projectile.timeLeft = 3;
+                p.ChangeDir(Projectile.direction);
+                p.heldProj = Projectile.whoAmI;
+                p.itemTime = 3;
+                p.itemAnimation = 3;
+                p.itemRotation = (Projectile.velocity * Projectile.direction).ToRotation();
+                Projectile.spriteDirection = Projectile.direction;
+            }
+
+            p.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.DirectionTo(Main.MouseWorld).ToRotation() - MathHelper.PiOver2);
+            Vector2 ArmPosition = p.RotatedRelativePoint(p.MountedCenter, false, false);
+            float RotationOffset = Utils.GetLerpValue(5, 255, Projectile.Distance(Main.MouseWorld), true);
+            Projectile.velocity = Vector2.Lerp(Projectile.velocity, Projectile.DirectionTo(Main.MouseWorld), RotationOffset);
+            Projectile.Center = ArmPosition;
+            Vector2 projPos = Projectile.Center += Projectile.velocity * 10;
+
+            #region swingPhysics
+            Vector2 dir = (Main.MouseWorld - barLoc).SafeNormalize(Vector2.Zero);
+
+            if (Vector2.Distance(barLoc, Main.MouseWorld) > 150)
+            {
+                barSpeed = 50f;
+            }
+            else
+                barSpeed = 20f;
+
+
+            if (Vector2.Distance(barLoc, Main.MouseWorld) > 15)
+            {
+                accBack = 0f;
+                accActualBack = 0.002f;
+                rotStop = false;
+                barLoc += dir * barSpeed;
+                if (acc < 1f)
+                    acc += 0.001f;
+                if (barLoc.X < Main.MouseWorld.X)
+                {
+                    if (barsRot < 0.5f)
+                    {
+                        barsRot += acc;
+                        rememberDir = 1;
+                    }
+                }
+
+                if (barLoc.X > Main.MouseWorld.X)
+                {
+                    if (barsRot > -0.5f)
+                    {
+                        barsRot -= acc;
+                        rememberDir = -1;
+                    }
+                }
+            }
+            else
+            {
+                acc = 0f;
+                if (accBack < 1f)
+                    accBack += 0.001f;
+
+                if (!rotStop)
+                {
+                    if (rememberDir == -1)
+                    {
+                        barsRot += accBack;
+                        if (barsRot > 0f)
+                        {
+                            accBack -= accActualBack;
+                            if (accActualBack > 0.0015f)
+                            {
+                                accActualBack -= 0.00001f;
+                            }
+                            else
+                            {
+                                rotStop = true;
+                                accActualBack = 0f;
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        barsRot -= accBack;
+                        if (barsRot < 0f)
+                        {
+                            accBack -= accActualBack;
+                            if (accActualBack > 0.0015f)
+                            {
+                                accActualBack -= 0.00001f;
+                            }
+                            else
+                            {
+                                rotStop = true;
+                                accActualBack = 0f;
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+
+                    accActualBack += 0.001f;
+                    if (barsRot > 0f)
+                    {
+                        barsRot -= accActualBack;
+                    }
+
+                    if (barsRot < 0f)
+                    {
+                        barsRot += accActualBack;
+                    }
+                }
+
+
+                barLoc = Main.MouseWorld;
+            }
+            #endregion
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D tex = ModContent.Request<Texture2D>(Mod.Name + "/Content/Items/Weapons/Underworld/Marionette").Value;
+            Rectangle sourceRect = new Rectangle(0, 0, tex.Width, tex.Height);
+
+            Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, sourceRect, Color.Gray, Projectile.rotation, sourceRect.Size() / 2, 1f, Projectile.direction == 1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+
+            Vector2 barsLoc = new Vector2(Main.MouseWorld.X, Main.MouseWorld.Y + 16) - Main.screenPosition;
+
+            Vector2 verlet1Pos = new Vector2(barsLoc.X - 28, (barsLoc.Y + barsRot) - 6);
+            verlet1Pos.Y += -40 * barsRot;
+            Vector2 actualVerlet1EndPos = new Vector2(verlet1Pos.X, verlet1Pos.Y + 300);
+            if (verlet1EndPos == Vector2.Zero)
+                verlet1EndPos = actualVerlet1EndPos;
+
+            Vector2 direction = (actualVerlet1EndPos - verlet1EndPos).SafeNormalize(Vector2.Zero);
+            Vector2 projVelocity = direction * 5f;
+            verlet1EndPos += projVelocity;
+
+            DrawVerlet(verlet1Pos, verlet1EndPos);
+
+            tex = ModContent.Request<Texture2D>(Mod.Name + "/Content/Items/Weapons/Underworld/MarionetteBar").Value;
+            sourceRect = new Rectangle(0, 0, tex.Width, tex.Height);
+
+            Main.EntitySpriteDraw(tex, barsLoc, sourceRect, Color.Gray, 0.6f + barsRot, sourceRect.Size() / 2, 1f, SpriteEffects.None);
+            Main.EntitySpriteDraw(tex, barsLoc, sourceRect, Color.White, 0f + barsRot, sourceRect.Size() / 2, 1f, SpriteEffects.None);
+
+            return false;
+        }
+
+        #region DrawVerlet
+
+        public void DrawVerlet(Vector2 verletPos1, Vector2 verletPos2)
+        {
+            Rectangle pixelSource = new Rectangle(0, 0, 2, 2);
+
+            float calcDist = 1f;
+            Vector2 dir = (verletPos2 - verletPos1).SafeNormalize(Vector2.Zero);
+            for (int i = 0; i < 5000; i++)
+            {
+                Vector2 stepCalcPos = verletPos1 + (dir * calcDist);
+                if (Vector2.Distance(stepCalcPos, verletPos2) < 1)
+                {
+                    break;
+                }
+                calcDist += 1f;
+            }
+
+            List<Vector2> points = new List<Vector2>();
+            points.Add(verletPos1);
+
+            #region MiddlePoints (done manually so it looks nice)
+            Vector2 middlePoint = verletPos1 + (dir * (calcDist * 0.15f));
+            middlePoint.Y += 16;
+
+            middlePoint = verletPos1 + (dir * (calcDist * 0.25f));
+            middlePoint.Y += 24;
+            points.Add(middlePoint);
+
+            middlePoint = verletPos1 + (dir * (calcDist * 0.35f));
+            middlePoint.Y += 30;
+            points.Add(middlePoint);
+
+            middlePoint = verletPos1 + (dir * (calcDist * 0.45f));
+            middlePoint.Y += 35;
+            points.Add(middlePoint);
+
+            middlePoint = verletPos1 + (dir * (calcDist * 0.5f));
+            middlePoint.Y += 35;
+            points.Add(middlePoint);
+
+            middlePoint = verletPos1 + (dir * (calcDist * 0.65f));
+            middlePoint.Y += 35;
+            points.Add(middlePoint);
+
+            middlePoint = verletPos1 + (dir * (calcDist * 0.75f));
+            middlePoint.Y += 32;
+            points.Add(middlePoint);
+
+            middlePoint = verletPos1 + (dir * (calcDist * 0.85f));
+            middlePoint.Y += 24;
+            points.Add(middlePoint);
+
+            middlePoint = verletPos1 + (dir * (calcDist * 0.95f));
+            middlePoint.Y += 8;
+            points.Add(middlePoint);
+            #endregion
+
+            points.Add(verletPos2);
+
+            for (int s = 0; s < points.Count - 1; s++)
+            {
+
+                float progress = 0f;
+                for (int i = 0; i < 5000; i++)
+                {
+                    Vector2 startPos = points[s];
+                    Vector2 endPos = points[(s + 1)];
+                    Vector2 direction = (endPos - startPos).SafeNormalize(Vector2.Zero);
+                    Vector2 progPoint = startPos + (direction * progress);
+                    Main.EntitySpriteDraw(TextureAssets.MagicPixel.Value, progPoint, pixelSource, Color.White, 0f, pixelSource.Size() / 2, 1f, SpriteEffects.None);
+                    if (Vector2.Distance(progPoint, endPos) < 1)
+                    {
+                        break;
+                    }
+                    progress += 1f;
+                }
+
+            }
+
+        }
+        #endregion
+    }
+}
