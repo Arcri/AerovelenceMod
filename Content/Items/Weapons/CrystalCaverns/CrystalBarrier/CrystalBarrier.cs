35772641fc1ebc601204dc2d49a480f0b081018b
Scratchock <the.maker.of.scratchock.live@gmail.com>
2025-08-25 12:29:08 -0400

    New early game sentry
    Now featuring a badly made sentry that needs a lot of touchups to be worth implementing into the CCdate

---
+++ Content/Items/Weapons/CrystalCaverns/CrystalBarrier/CrystalBarrier.cs
@@ -0,0 +1,81 @@
+﻿using AerovelenceMod.Content.Buffs.PlayerInflictedDebuffs;
+using AerovelenceMod.Content.Items.Weapons.CrystalCaverns.CrystalBarrier;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AerovelenceMod.Content.Items.Weapons.CrystalCaverns.CrystalBarrier
+{
+    internal class CrystalBarrier : ModProjectile
+    {
+
+        public override void SetStaticDefaults()
+        {
+
+            Main.projFrames[Projectile.type] = 5;
+        }
+        public override void SetDefaults()
+        {
+            Projectile.width = 26;
+            Projectile.height = 60; //it should be a lot taller/bigger than this, i just put together a VERY quick placeholder sprite without considering size
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.sentry = true; //Sets the weapon as a sentry for sentry accessories to properly work.
+            Projectile.timeLeft = Projectile.SentryLifeTime;
+            Projectile.ignoreWater = true; //If this is set to false, the projectile will be slowed in water.
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.usesLocalNPCImmunity = false;
+        }
+        public int CooldownTimer = 0;
+        public override void AI()
+        {
+            //This AI will function as a static sentry, and will not move. If you would like to know how to do more advanced minion AI, check out PurityWisp.cs.
+
+            Main.player[Projectile.owner].UpdateMaxTurrets(); //This makes the sentry be able to spawn more if your sentry cap is greater than one.
+            //Animate the projectile.
+            Projectile.frameCounter++;
+            //10 here changes frame rate (60/10, so 6 fps here), and stops updating after 60
+            if (Projectile.frameCounter % 10 == 0 && Projectile.frameCounter < 50)
+            {
+                Projectile.frame++;
+            }
+            if (CooldownTimer >= 0)
+            {
+                CooldownTimer--;
+                if (CooldownTimer >= 10 && CooldownTimer <= 25 || CooldownTimer >= 35 && CooldownTimer <= 50 || CooldownTimer >= 60 && CooldownTimer <= 75)
+                {
+                    Projectile.alpha = 0;
+                }
+                else if (CooldownTimer >= 0)
+                {
+                    {
+                        Projectile.alpha = 80; //just a very simple transparency pass, but i'm sure there's a better way to make this cooler(think like literally mario 1 damage blinking)
+                    }
+                }
+            }
+            }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            CooldownTimer = 180; //could be even longer, mostly a default testing count
+        }
+        public override bool? CanDamage() //make it somehow hit one enemy at a time? gimmicky and not useful mechanic, but possible addition
+        {
+            if (CooldownTimer >= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
+
---
+++ Content/Items/Weapons/CrystalCaverns/CrystalBarrier/CrystalBarrierRaiser.cs
@@ -0,0 +1,85 @@
+﻿using AerovelenceMod.Common.Systems.Language;
+using AerovelenceMod.Common.Utilities;
+using AerovelenceMod.Content.Items.Sets.Phantic;
+using AerovelenceMod.Content.Items.Weapons.CrystalCaverns.CrystalBarrier;
+using AerovelenceMod.Content.Tiles.CrystalCaverns.Glimmerwood;
+using AerovelenceMod.Content.Tiles.CrystalCaverns.Natural;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AerovelenceMod.Content.Items.Weapons.CrystalCaverns.CrystalBarrier
+{
+    internal class CrystalBarrierRaiser : TranslatableModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.GamepadWholeScreenUseRange[Item.type] = true;
+            ItemID.Sets.LockOnIgnoresCollision[Item.type] = true;
+            this.ModifyLocalization("Crystal Thorn-Barrier Raiser", "(placeholder description, final item should not have a description as it is very simplistic and easy to craft) Summons a weak barrier to protect from enemies with large amounts of kb. Can also be used in a spam playstyle as an active move unlike standard summoner passive gameplay.")
+            .AddName(Language.Default, "Crystal Thorn-Barrier Raiser")
+            .AddTooltip(Language.Default, "(placeholder description, final item should not have a description as it is very simplistic and easy to craft) \nSummons a weak barrier to protect from enemies with large amounts of kb. \nCan also be used in a spam playstyle as an active move unlike standard summoner passive gameplay by ignoring the cooldown and resummoning.");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 14;
+            Item.sentry = true;
+            Item.mana = 8; //How much mana this weapon takes to use.
+            Item.width = 28; //Item width hitbox.
+            Item.height = 26; //Item height hitbox.
+            Item.useTime = 22;
+            Item.useAnimation = 22;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.noMelee = true; //Restricts this weapon dealing melee damage.
+            Item.knockBack = 9;
+            Item.value = Item.buyPrice(0, 20, 0, 0); //How much this item is sold for.
+            Item.rare = ItemRarityID.Cyan;
+            Item.UseSound = SoundID.Item83;
+            Item.shoot = ModContent.ProjectileType<CrystalBarrier>();
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<CavernCrystalItem>(), 5)
+                .AddIngredient(ModContent.ItemType<GlimmerwoodItem>(), 16)
+                .AddTile(TileID.WorkBenches)
+                .Register();
+        }
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+        {
+            base.ModifyShootStats(player, ref position, ref velocity, ref type, ref damage, ref knockback);
+            FindSentryRestingSpot(player, type, out position);
+            return;
+
+        }
+        public static void FindSentryRestingSpot(Player player, int checkProj, out Vector2 position)
+        {
+            player.FindSentryRestingSpot(checkProj, out int worldX, out int worldY, out int pushYUp);
+            position = new Vector2(worldX, worldY);
+
+            if (checkProj <= -1 || checkProj >= ProjectileLoader.ProjectileCount)
+            {
+                position.Y -= pushYUp;
+                return;
+            }
+
+            Projectile p = ContentSamples.ProjectilesByType[checkProj];
+            pushYUp = (int)Math.Ceiling(p.height / 2f); //Always round up in case height is not even
+
+            position.Y -= pushYUp;
+        }
+    }
+
+}
+
+
+
+
+
+
+
